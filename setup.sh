adfe4063164b1ec8ac32b3f0945da84cc709ae78
bee-rickey <bee.ricketyship@gmail.com>
2022-07-20 17:33:08 +0530

    Adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eu
+
+repo_uri="https://x-access-token:${MY_TOKEN}@github.com/travellogger.git"
+remote_name="origin"
+main_branch="master"
+gh_pages_branch="gh-pages"
+
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "${GITHUB_ACTOR}@bots.github.com"
+
+git checkout "$main_branch"
+
+python3 start.py
+
+git add .
+set +e  # Grep succeeds with nonzero exit codes to show results.
+
+if git status | grep 'new file\|modified'
+then
+    set -e
+    git commit -am "data updated on - $(date)"
+    git remote set-url "$remote_name" "$repo_uri" # includes access token
+    git push --force-with-lease "$remote_name" "$main_branch"
+else
+    set -e
+    echo "No changes since last run"
+fi
+
+echo "finish"
